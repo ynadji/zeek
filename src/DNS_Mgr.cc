6e3f99968313de575472d74d40b9f6a64fde7aa4
Jon Siwek <jsiwek@corelight.com>
2020-05-05 10:40:55 -0700

    Deprecate ListVal::ConvertToSet(), add ListVal::ToSetVal()

--- src/DNS_Mgr.cc
+++ src/DNS_Mgr.cc
@@ -297,7 +297,7 @@ IntrusivePtr<TableVal> DNS_Mapping::AddrsSet() {
 	if ( ! l )
 		return empty_addr_set();
 
-	return {AdoptRef{}, l->ConvertToSet()};
+	return l->ToSetVal();
 	}
 
 IntrusivePtr<StringVal> DNS_Mapping::Host()
@@ -480,7 +480,7 @@ static IntrusivePtr<TableVal> fake_name_lookup_result(const char* name)
 	    reinterpret_cast<u_char*>(hash));
 	auto hv = make_intrusive<ListVal>(TYPE_ADDR);
 	hv->Append(new AddrVal(hash));
-	return {AdoptRef{}, hv->ConvertToSet()};
+	return hv->ToSetVal();
 	}
 
 static const char* fake_text_lookup_result(const char* name)
@@ -716,11 +716,7 @@ void DNS_Mgr::Event(EventHandlerPtr e, DNS_Mapping* dm,
 	if ( ! e )
 		return;
 
-	mgr.Enqueue(e,
-		BuildMappingVal(dm),
-		IntrusivePtr{AdoptRef{}, l1->ConvertToSet()},
-		IntrusivePtr{AdoptRef{}, l2->ConvertToSet()}
-	);
+	mgr.Enqueue(e, BuildMappingVal(dm), l1->ToSetVal(), l2->ToSetVal());
 	}
 
 void DNS_Mgr::Event(EventHandlerPtr e, DNS_Mapping* old_dm, DNS_Mapping* new_dm)
