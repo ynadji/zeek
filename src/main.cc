c0e9a0a6f8fddb7a81ee794394a3b8c73b10834f
Jon Siwek <jsiwek@corelight.com>
2020-05-08 18:43:56 -0700

    Deprecate internal_val() and internal_const_val()
    Replaced with zeek::lookup_val() and zeek::lookup_const()

--- src/main.cc
+++ src/main.cc
@@ -361,7 +361,7 @@ static std::vector<std::string> get_script_signature_files()
 
 	// Parse rule files defined on the script level.
 	char* script_signature_files =
-		copy_string(internal_val("signature_files")->AsString()->CheckString());
+		copy_string(zeek::lookup_val("signature_files")->AsString()->CheckString());
 
 	char* tmp = script_signature_files;
 	char* s;
@@ -752,7 +752,7 @@ int main(int argc, char** argv)
 
 	if ( ! options.pcap_file && ! options.interface )
 		{
-		Val* interfaces_val = internal_val("interfaces");
+		const auto& interfaces_val = zeek::lookup_val("interfaces");
 		if ( interfaces_val )
 			{
 			char* interfaces_str =
